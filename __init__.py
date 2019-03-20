06e09755314076afd01b8da508f8871d3c0a4640
TobiasMichael96 <tobiasmichael@mail.de>
2019-03-20 11:02:46 +0100

    Changed skill to accept simple toggle command

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import paho.mqtt.publish as publish
 
 from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill
 
 
 class SonoffToggle(MycroftSkill):
@@ -12,12 +12,14 @@ class SonoffToggle(MycroftSkill):
         self.__build_single_command()
 
     def __build_single_command(self):
-        intent = IntentBuilder("mymqttIntent").require("CommandKeyword").require("ModuleKeyword").require("ActionKeyword").build()
+        intent = IntentBuilder("mymqttIntent").require("CommandKeyword").require("ModuleKeyword").build()
         self.register_intent(intent, self.handle_toggle_sonoff)
 
     def handle_toggle_sonoff(self, message):
         dev_name = message.data.get("ModuleKeyword").replace(' ', '_')
-        act_name = message.data.get("ActionKeyword").replace(' ', '_')
+        act_name = message.data.get("ActionKeyword")
+        if message.data.get("CommandKeyword") == "toggle":
+                act_name = "toggle"
 
         publish.single("cmnd/" + dev_name + "/power", act_name, hostname="127.0.0.1")
 
