0a93e6d6d2fa90283e24a8445e6df4e6dd5e79f2
root <root@homeserver>
2019-03-22 13:24:54 +0100

    Reformatted python file

--- __init__.py
+++ __init__.py
@@ -12,17 +12,14 @@ class SonoffToggle(MycroftSkill):
         toggle_intent = IntentBuilder("mymqttIntent").require("CommandKeyword").require("ModuleKeyword").optionally("ActionKeyword").build()
         self.register_intent(toggle_intent, self.handle_toggle_sonoff)
 
-
     def handle_toggle_sonoff(self, message):
         dev_name = message.data.get("ModuleKeyword").replace(' ', '_')
-        if  message.data.get("ActionKeyword"):
+        if message.data.get("ActionKeyword"):
                 act_name = message.data.get("ActionKeyword")
         else:
                 act_name = "toggle"
-
         publish.single("cmnd/" + dev_name + "/power", act_name, hostname="127.0.0.1")
         self.speak_dialog('toggle.sonoff')
-
 
 def create_skill():
     return SonoffToggle()
